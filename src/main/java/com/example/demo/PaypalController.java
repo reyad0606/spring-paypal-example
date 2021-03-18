182e98bf6e49c746f1160a9ed55cced4d1967bec
RM <70502707+reyad0606@users.noreply.github.com>
2021-03-18 15:52:22 -0400

    commit

--- src/main/java/com/example/demo/PaypalController.java
+++ src/main/java/com/example/demo/PaypalController.java
@@ -1,9 +1,16 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PaypalController {
 
+		@Autowired
 		PaypalService service;
+		
+		@GetMapping("/")
+		public String home() {
+			return "home";
+		}
 }
